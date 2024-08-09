872c2a13223363c2b9753f408763537237d7c576
Ezgi <ezgycl28@gmail.com>
2024-08-09 14:39:28 +0300

    Add files via upload

---
+++ Purple_Kutphane_Sistemi/Purple_Kutphane_Sistemi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Purple_Kutphane_Sistemi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
