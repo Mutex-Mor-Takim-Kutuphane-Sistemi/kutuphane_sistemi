d28b2e3c1257e5ad045354e19cf6646f80ed94f1
Ezgi <ezgycl28@gmail.com>
2024-08-09 18:21:01 +0300

    Data klasörü açıldı bağlantı kuruldu program.js apsettings.json dosyalarında değişiklikler yapıldı

---
+++ Purple_Kutphane_Sistemi/Purple_Kutphane_Sistemi/Data/DbBaglanti.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace Purple_Kutphane_Sistemi.Data
+{
+
+    public class DbBaglanti : DbContext
+    {
+        public DbBaglanti(DbContextOptions<DbBaglanti> options) : base(options)
+        {
+
+        }
+    }
+}
--- Purple_Kutphane_Sistemi/Purple_Kutphane_Sistemi/Program.cs
+++ Purple_Kutphane_Sistemi/Purple_Kutphane_Sistemi/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Purple_Kutphane_Sistemi.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbBaglanti>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
