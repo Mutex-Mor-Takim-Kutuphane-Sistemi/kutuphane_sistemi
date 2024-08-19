c67e3d1eab6d2a06d4c650cedac912fc547d2087
Agahtkr <agaherent@gmail.com>
2024-08-19 20:35:00 +0300

    Kullanıcı İşlemleri
    Kullanıcıyla ilgili işlemleri (kayıt olma, giriş yapma) kabaca yaptım, modifiye edilebilir.

---
+++ Purple_Kutphane_Sistemi/Purple_Kutphane_Sistemi/Kullanici_İslemleri.cs
@@ -0,0 +1,65 @@
+﻿using Purple_Kutphane_Sistemi.Data;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Purple_Kutphane_Sistemi
+{
+    public class Kullanici_İslemleri
+    {
+        private readonly DbBaglanti _context;
+
+        public Kullanici_İslemleri(DbBaglanti context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> KayitOl(Kullanici yeniKullanici)
+        {
+            var mevcutKullanici = await _context.Kullanicilar
+                .FirstOrDefaultAsync(k => k.Email == yeniKullanici.Email);
+
+            if (mevcutKullanici != null)
+            {
+                return false;
+            }
+
+            
+            yeniKullanici.Parola = HashPassword(yeniKullanici.Parola); // İnanılmaz bir sistem
+
+            yeniKullanici.KayıtTarihi = DateTime.Now;
+
+            _context.Kullanicilar.Add(yeniKullanici);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        private string HashPassword(string password)
+        {
+            return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        public async Task<Kullanici> GirisYap(string email, string parola)
+        {
+            var kullanici = await _context.Kullanicilar
+                .FirstOrDefaultAsync(k => k.Email == email);
+
+            if (kullanici == null)
+            {
+                return null;
+            }
+
+            var hashliParola = HashPassword(parola);
+
+            if (kullanici.Parola != hashliParola)
+            {
+                return null;
+            }
+
+            else
+            return kullanici;
+        }
+
+    }
+}
